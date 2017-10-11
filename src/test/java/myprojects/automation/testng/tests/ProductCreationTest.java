9adb7a6f773b9a4e707ac59c94a7f8a077ad151f
RoxVer <rshveda15@gmail.com>
2017-10-11 21:22:04 +0300

    Wrapped driver for logging

--- src/test/java/myprojects/automation/testng/tests/ProductCreationTest.java
+++ src/test/java/myprojects/automation/testng/tests/ProductCreationTest.java
@@ -7,9 +7,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Reporter;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -18,6 +20,7 @@ import org.testng.asserts.SoftAssert;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 
 /**
@@ -27,6 +30,7 @@ import java.util.Random;
  */
 public class ProductCreationTest {
     WebDriver driver;
+    EventFiringWebDriver wrappedDriver;
     Random rand = new Random();
     String randName;
     int randomQuantity;
@@ -47,18 +51,26 @@ public class ProductCreationTest {
             System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
             driver = new ChromeDriver();
         }
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        wrappedDriver = new EventFiringWebDriver(driver);
+        wrappedDriver.register(new EventHandler());
     }*/
 
     @BeforeClass
     public void setup() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        wrappedDriver = new EventFiringWebDriver(driver);
+        wrappedDriver.register(new EventHandler());
     }
 
-    /*@AfterClass
+    @AfterClass
     public void afterClass() {
         driver.quit();
-    }*/
+    }
 
     @DataProvider
     public Object[][] getLoginData() {
@@ -69,37 +81,37 @@ public class ProductCreationTest {
 
     @Test (dataProvider = "getLoginData")
     public void loginTest(String email, String password) {
-        driver.navigate().to("http://prestashop-automation.qatestlab.com.ua/admin147ajyvk0/");
-        driver.findElement(By.id("email")).sendKeys(email);
-        driver.findElement(By.id("passwd")).sendKeys(password);
-        driver.findElement(By.name("submitLogin")).click();
+        wrappedDriver.navigate().to("http://prestashop-automation.qatestlab.com.ua/admin147ajyvk0/");
+        wrappedDriver.findElement(By.id("email")).sendKeys(email);
+        wrappedDriver.findElement(By.id("passwd")).sendKeys(password);
+        wrappedDriver.findElement(By.name("submitLogin")).click();
 
-        WebDriverWait wait = new WebDriverWait(driver, 5);
+        WebDriverWait wait = new WebDriverWait(wrappedDriver, 5);
         wait.until(ExpectedConditions.titleContains("Dashboard"));
     }
 
     @Test(dependsOnMethods = "loginTest")
     public void checkProductCreation() {
         Reporter.log("Open Catalog section <br />");
-        WebElement catalogTabElement = driver.findElement(By.id("subtab-AdminCatalog"));
-        Actions actions = new Actions(driver);
+        WebElement catalogTabElement = wrappedDriver.findElement(By.id("subtab-AdminCatalog"));
+        Actions actions = new Actions(wrappedDriver);
         actions.moveToElement(catalogTabElement).build().perform();
         catalogTabElement.findElements(By.cssSelector("li")).get(0).click();
-        driver.findElement(By.id("page-header-desc-configuration-add")).click();
-        driver.findElement(By.name("form[step1][name][1]")).sendKeys(getProductName(10));
-        driver.findElement(By.id("form_step1_qty_0_shortcut")).sendKeys(Integer.toString(randomQuantity()));
-        driver.findElement(By.id("form_step1_price_shortcut")).sendKeys(Double.toString(randomPrice()));
-        driver.findElement(By.cssSelector("div.col-lg-5 > div")).click();
+        wrappedDriver.findElement(By.id("page-header-desc-configuration-add")).click();
+        wrappedDriver.findElement(By.name("form[step1][name][1]")).sendKeys(getProductName(10));
+        wrappedDriver.findElement(By.id("form_step1_qty_0_shortcut")).sendKeys(Integer.toString(randomQuantity()));
+        wrappedDriver.findElement(By.id("form_step1_price_shortcut")).sendKeys(Double.toString(randomPrice()));
+        wrappedDriver.findElement(By.cssSelector("div.col-lg-5 > div")).click();
         verifyAlert();
-        driver.findElement(By.cssSelector("div > button.btn.btn-primary.js-btn-save"));
+        wrappedDriver.findElement(By.cssSelector("div > button.btn.btn-primary.js-btn-save"));
         verifyAlert();
     }
 
     @Test(dependsOnMethods = "checkProductCreation")
     public void verifyCreatedProduct() {
-        driver.navigate().to("http://prestashop-automation.qatestlab.com.ua/");
-        driver.findElement(By.cssSelector("#content > section > a")).click();
-        WebElement element = driver.findElement(By.xpath("//*[@id=\"js-product-list-top\"]/div[1]/p"));
+        wrappedDriver.navigate().to("http://prestashop-automation.qatestlab.com.ua/");
+        wrappedDriver.findElement(By.cssSelector("#content > section > a")).click();
+        WebElement element = wrappedDriver.findElement(By.xpath("//*[@id=\"js-product-list-top\"]/div[1]/p"));
         String prod = element.getText();
         prod = prod.replaceAll("\\D+","");
         int productsQ = 0;
@@ -109,7 +121,7 @@ public class ProductCreationTest {
             Reporter.log("Number Format Exception <br />");
         }
         for (int i = 0; i < productsQ; i++) {
-            List<WebElement> elements = driver.findElements(By.xpath("//h1/a"));
+            List<WebElement> elements = wrappedDriver.findElements(By.xpath("//h1/a"));
             products.add(elements.get(i).getText());
         }
         if (products.contains(randName)) {
@@ -118,14 +130,14 @@ public class ProductCreationTest {
             Reporter.log("Created product was NOT found <br />");
         }
 
-        WebDriverWait wait = new WebDriverWait(driver, 5);
+        WebDriverWait wait = new WebDriverWait(wrappedDriver, 5);
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h1/a[.='" + randName + "']")));
-        driver.findElement(By.xpath("//h1/a[.='" + randName + "']")).click();
-        WebElement nameEl = driver.findElement(By.cssSelector("div.row > div:nth-child(2) > h1"));
+        wrappedDriver.findElement(By.xpath("//h1/a[.='" + randName + "']")).click();
+        WebElement nameEl = wrappedDriver.findElement(By.cssSelector("div.row > div:nth-child(2) > h1"));
         String name = nameEl.getText();
         softAssertion.assertEquals(name, randName, "Incorrect name: ");
 
-        String priceEl = driver.findElement(By.cssSelector("div.product-price.h5 > div > span")).getText();
+        String priceEl = wrappedDriver.findElement(By.cssSelector("div.product-price.h5 > div > span")).getText();
         priceEl = priceEl.replaceAll("\\D+","");
         int price = 0;
         try {
@@ -135,7 +147,7 @@ public class ProductCreationTest {
         }
         softAssertion.assertEquals(price, randomPrice, "Incorrect price: ");
 
-        String quantityEl = driver.findElement(By.xpath("//*[@id=\"product-details\"]/div[1]/span")).getText();
+        String quantityEl = wrappedDriver.findElement(By.xpath("//*[@id=\"product-details\"]/div[1]/span")).getText();
         quantityEl = quantityEl.replaceAll("\\D+","");
         int quantity = 0;
         try {
@@ -149,9 +161,9 @@ public class ProductCreationTest {
 
     public void verifyAlert() {
         try {
-            WebDriverWait wait = new WebDriverWait(driver, 2);
+            WebDriverWait wait = new WebDriverWait(wrappedDriver, 2);
             wait.until(ExpectedConditions.alertIsPresent());
-            Alert alert = driver.switchTo().alert();
+            Alert alert = wrappedDriver.switchTo().alert();
             String message = alert.getText();
             softAssertion.assertEquals(message, "Настройки обновлены.", "Alert message is incorrect");
             alert.dismiss();
